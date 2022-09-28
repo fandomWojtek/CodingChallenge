266e3b056662da27d359043cf9112da383466483
wojtek maciejewski <wojciechmaciejewski1985@gmail.com>
2022-09-28 19:57:09 +0200

    add some assets to project

--- app/src/main/java/com/fandom/coding/apis.kt
+++ app/src/main/java/com/fandom/coding/apis.kt
@@ -4,15 +4,15 @@ import android.os.Looper
 import kotlin.random.Random
 
 class ColorApi {
-     fun loadColors(): List<NetworkColor> {
-         if(Looper.getMainLooper().isCurrentThread){
-              throw IllegalStateException("CANNOT ACCESS ON MAIN THREAD")
-         }
+    fun loadColors(): List<NetworkColor> {
+        if (Looper.getMainLooper().isCurrentThread) {
+            throw IllegalStateException("CANNOT ACCESS ON MAIN THREAD")
+        }
         return listOf(
-            NetworkColor(1, "#FF00FF"),
-            NetworkColor(2, "#FF0000"),
-            NetworkColor(3, "#0000FF"),
-            NetworkColor(4, "#BB00FF")
+            NetworkColor(colorCode = 1, color = "#FF00FF"),
+            NetworkColor(colorCode = 2, color = "#FF0000"),
+            NetworkColor(colorCode = 3, color = "#0000FF"),
+            NetworkColor(colorCode = 4, color = "#BB00FF")
         )
     }
 }
@@ -20,9 +20,9 @@ class ColorApi {
 
 class FigureApi {
     fun loadFigures(): List<NetworkFigure> {
-         if(Looper.getMainLooper().isCurrentThread){
-              throw IllegalStateException("CANNOT ACCESS ON MAIN THREAD")
-         }
+        if (Looper.getMainLooper().isCurrentThread) {
+            throw IllegalStateException("CANNOT ACCESS ON MAIN THREAD")
+        }
         return listOfListOfFigures[Random.nextInt(3)]
     }
 }
@@ -32,29 +32,29 @@ private const val TRIANGLE = "triangle"
 private const val RECTANGLE = "rectangle"
 private const val DIAMOND = "diamond"
 
-private  val listOfListOfFigures = listOf(
+private val listOfListOfFigures = listOf(
     listOf(
-        NetworkFigure(type = SQUARE, colorCode = 1, 1),
-        NetworkFigure(type = SQUARE, colorCode = 2, 2),
-        NetworkFigure(type = TRIANGLE, colorCode = 2, 3),
-        NetworkFigure(type = DIAMOND, colorCode = 3, 4),
-        NetworkFigure(type = RECTANGLE, colorCode = 4, 5),
-        NetworkFigure(type = TRIANGLE, colorCode = 4, 6)
+        NetworkFigure(type = SQUARE, colorCode = 1, id = 1),
+        NetworkFigure(type = SQUARE, colorCode = 2, id = 2),
+        NetworkFigure(type = TRIANGLE, colorCode = 2, id = 3),
+        NetworkFigure(type = DIAMOND, colorCode = 3, id = 4),
+        NetworkFigure(type = RECTANGLE, colorCode = 4, id = 5),
+        NetworkFigure(type = TRIANGLE, colorCode = 4, id = 6)
     ),
     listOf(
-        NetworkFigure(type = SQUARE, colorCode = 1, 3),
-        NetworkFigure(type = SQUARE, colorCode = 1, 2),
-        NetworkFigure(type = TRIANGLE, colorCode = 3, 1),
-        NetworkFigure(type = DIAMOND, colorCode = 3, 4),
-        NetworkFigure(type = RECTANGLE, colorCode = 2, 6),
-        NetworkFigure(type = TRIANGLE, colorCode = 4, 5),
+        NetworkFigure(type = SQUARE, colorCode = 1, id = 3),
+        NetworkFigure(type = SQUARE, colorCode = 1, id = 2),
+        NetworkFigure(type = TRIANGLE, colorCode = 3, id = 1),
+        NetworkFigure(type = DIAMOND, colorCode = 3, id = 4),
+        NetworkFigure(type = RECTANGLE, colorCode = 2, id = 6),
+        NetworkFigure(type = TRIANGLE, colorCode = 4, id = 5),
     ),
     listOf(
-        NetworkFigure(type = TRIANGLE, colorCode = 1, 1),
-        NetworkFigure(type = SQUARE, colorCode = 2, 4),
-        NetworkFigure(type = TRIANGLE, colorCode = 2, 5),
-        NetworkFigure(type = DIAMOND, colorCode = 3, 2),
-        NetworkFigure(type = RECTANGLE, colorCode = 4, 3),
-        NetworkFigure(type = DIAMOND, colorCode = 4, 6),
+        NetworkFigure(type = TRIANGLE, colorCode = 1, id = 1),
+        NetworkFigure(type = SQUARE, colorCode = 2, id = 4),
+        NetworkFigure(type = TRIANGLE, colorCode = 2, id = 5),
+        NetworkFigure(type = DIAMOND, colorCode = 3, id = 2),
+        NetworkFigure(type = RECTANGLE, colorCode = 4, id = 3),
+        NetworkFigure(type = DIAMOND, colorCode = 4, id = 6),
     )
 )
